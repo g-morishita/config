f1bd572fbbb49c023606e9b07c6d940bc713cfc1
gota morishita <optimistic.gota@gmail.com>
2026-03-02 04:35:25 +0000

    add nvim config

---
+++ nvim/lua/plugins/nostr-nvim.lua
@@ -0,0 +1,8 @@
+return {
+    enabled = false,
+	dir = "~/Repos/nostr.nvim/", -- Use local directory instead of GitHub
+	build = "npm run build", -- Build on install/update
+	config = function()
+		-- Optional: set up keymaps or other config
+	end,
+}
